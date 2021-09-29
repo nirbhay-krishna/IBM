e105fb418da2d843e38d6b8db4430acd6f0c3aad
Nirbhay Krishna <65804844+nirbhay-krishna@users.noreply.github.com>
2021-09-29 23:28:55 +0530

    Add files via upload

---
+++ Assignment2.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Assignment2.aspx.cs" Inherits="WebApplication2.Assignment2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" DataKeyNames="Empid" runat="server" AutoGenerateColumns="False" EnableModelValidation="True"
+               ShowFooter="true" EmptyDataText="No Records Found" OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                OnRowCommand="GridView1_RowCommand" OnRowDataBound="GridView1_RowDataBound" 
+                OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" >
+                <Columns>
+                    <asp:TemplateField HeaderText="Emp Name">
+                        <ItemTemplate>
+                            <asp:Label ID="lblEname" runat="server" Text='<%# Eval("EmpName")%>' />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox id="txtEditEname" runat="server" PlaceHolder="Enter Emp Name" 
+                                Text='<%# Eval("EmpName") %>' />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                              <asp:TextBox id="txtEname" runat="server" PlaceHolder="Enter Emp Name"  />
+                        </FooterTemplate>
+                    </asp:TemplateField>                
+                    <asp:TemplateField HeaderText="Salary">
+                        <ItemTemplate>
+                            <asp:Label ID="lblSalary" runat="server" Text='<%# Eval("Salary")%>' />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox id="txtEditSalary" runat="server" PlaceHolder="Enter Salary" 
+                                Text='<%# Eval("Salary") %>' />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                              <asp:TextBox id="txtSalary" runat="server" PlaceHolder="Enter Salary"  />
+                        </FooterTemplate>
+                    </asp:TemplateField> 
+                    <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                            <asp:LinkButton runat="server" ID="btnEdit" Text="Edit" CommandName="Edit" ToolTip="Edit" />
+                            <asp:LinkButton runat="server" ID="btnDelete" Text="Delete" CommandName="Delete" ToolTip="Delete" />
+                            </ItemTemplate>
+                        <EditItemTemplate>
+                              <asp:LinkButton runat="server" ID="btnUpdate" Text="Update" CommandName="Update" ToolTip="Update" />
+                            <asp:LinkButton runat="server" ID="btnCancel" Text="Cancel" CommandName="Cancel" ToolTip="Cancel" />                      
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:Button ID="btnAdd"  CommandName="AddNew" runat="server" Text="Add Record" />
+                        </FooterTemplate>
+                        </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Assignment2.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class Assignment2 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"server=LAPTOP-LTE8ELEI;Integrated Security=SSPI;database=ibmdb");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bindgrid();
+            }
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            Bindgrid();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("AddNew"))
+            {
+                TextBox inempname = (TextBox)GridView1.FooterRow.FindControl("txtEname");
+                TextBox insalary = (TextBox)GridView1.FooterRow.FindControl("txtSalary");
+
+                con.Open();
+                String strcmd = "insert into Emp (empname,salary) values('" + inempname.Text + "'," + insalary.Text + ")";
+                SqlCommand cmd = new SqlCommand(strcmd, con);
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    Bindgrid();
+                    lblMsg.BackColor = System.Drawing.Color.Green;
+                    lblMsg.ForeColor = System.Drawing.Color.White;
+                    lblMsg.Text = "Added Successfully";
+                }
+                else
+                {
+                    lblMsg.BackColor = System.Drawing.Color.Red;
+                    lblMsg.ForeColor = System.Drawing.Color.White;
+                    lblMsg.Text = "Error While Adding row";
+                }
+                con.Close();
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            Bindgrid();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string inempid = GridView1.DataKeys[e.RowIndex].Values["Empid"].ToString();
+            TextBox inempname = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtEditEname");
+            TextBox insalary = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtEditSalary");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update Emp set EmpName='" + inempname.Text + "', Salary=" + insalary.Text + " where empid=" + inempid, con);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+
+                lblMsg.BackColor = System.Drawing.Color.Green;
+                lblMsg.ForeColor = System.Drawing.Color.White;
+                lblMsg.Text = "Updated Successfully";
+                GridView1.EditIndex = -1;
+                Bindgrid();
+            }
+            else
+            {
+                lblMsg.BackColor = System.Drawing.Color.Red;
+                lblMsg.ForeColor = System.Drawing.Color.White;
+                lblMsg.Text = "Error While Adding row";
+            }
+            con.Close();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string inempid = GridView1.DataKeys[e.RowIndex].Values["Empid"].ToString();
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Delete from Emp where Empid=" + inempid, con);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+
+                lblMsg.BackColor = System.Drawing.Color.Green;
+                lblMsg.ForeColor = System.Drawing.Color.White;
+                lblMsg.Text = "Deleted Successfully";
+                Bindgrid();
+            }
+            else
+            {
+                lblMsg.BackColor = System.Drawing.Color.Red;
+                lblMsg.ForeColor = System.Drawing.Color.White;
+                lblMsg.Text = "Error While Adding row";
+            }
+            con.Close();
+        }
+        protected void Bindgrid()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from Emp", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            int count = ds.Tables[0].Rows.Count;
+            con.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+                int columcount = GridView1.Rows[0].Cells.Count;
+                lblMsg.Text = "No Data Found";
+
+            }
+        }
+    }
+}
